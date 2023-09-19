3d15855718472581bedb984def8e65f3f7894d2e
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-09-19 10:35:18 -0700

    refactor: icons are exported as raw text

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import bento from "./bento.svg?raw";
+import email from "./email.svg?raw";
+import github from "./github.svg?raw";
+import moon from "./moon.svg?raw";
+import sun from "./sun.svg?raw";
+import link from "./link.svg?raw";
+
+export { bento, email, github, moon, sun, link }
