391bc0674612db2d7858384519ddaf8a3ae2fd3f
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-09-19 21:12:34 -0700

    refactor: re-export the default import

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,6 @@
-import bento from "./bento.svg?raw";
-import email from "./email.svg?raw";
-import github from "./github.svg?raw";
-import moon from "./moon.svg?raw";
-import sun from "./sun.svg?raw";
-import link from "./link.svg?raw";
-
-export { bento, email, github, moon, sun, link }
+export { default as bento } from "./bento.svg?raw";
+export { default as email } from "./email.svg?raw";
+export { default as github } from "./github.svg?raw";
+export { default as moon } from "./moon.svg?raw";
+export { default as sun } from "./sun.svg?raw";
+export { default as link } from "./link.svg?raw";
